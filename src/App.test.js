c9194953fa613b9510facbdf04eed03408a07479
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-04-18 19:56:00 -0700

    first test successful

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,20 @@
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import { waitFor } from "@testing-library/react";
+import App from "./App.js"
 
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
+
+
+
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+  })
 
 
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,8 @@
+require('jest-fetch-mock').enableMocks()
+
+
+
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
